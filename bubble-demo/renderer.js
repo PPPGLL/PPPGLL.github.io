5124320a2741c33548314d0f04961365dbeb4e83
Gaole Pan <350086019@qq.com>
2026-08-07 14:10:17 +0800

    Revert "Prevent excessive overlap between bonded bubbles"
    This reverts commit 4cf2f1164625d15693247e15e063b7f180ad5da4.

--- bubble-demo/renderer.js
+++ bubble-demo/renderer.js
@@ -1,9 +1,9 @@
-import { v3, m3 } from "./math.js?v=20260807-13";
+import { v3, m3 } from "./math.js?v=20260807-12";
 import {
   createThinFilmLut,
   createFlowNoiseTexture,
   loadHdrTexture
-} from "./optics.js?v=20260807-13";
+} from "./optics.js?v=20260807-12";
 
 const ENVIRONMENTS = [
   "assets/envmap/sunny_vondelpark_4k.hdr"
