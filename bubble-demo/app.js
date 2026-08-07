4cf2f1164625d15693247e15e063b7f180ad5da4
Gaole Pan <350086019@qq.com>
2026-08-07 13:52:23 +0800

    Prevent excessive overlap between bonded bubbles

--- bubble-demo/app.js
+++ bubble-demo/app.js
@@ -1,6 +1,6 @@
-import { BubbleSimulation } from "./physics.js?v=20260807-12";
-import { BubbleRenderer } from "./renderer.js?v=20260807-12";
-import { rayFromScreen } from "./math.js?v=20260807-12";
+import { BubbleSimulation } from "./physics.js?v=20260807-13";
+import { BubbleRenderer } from "./renderer.js?v=20260807-13";
+import { rayFromScreen } from "./math.js?v=20260807-13";
 
 const canvas = document.querySelector("#scene");
 const errorScreen = document.querySelector("#error");
--- bubble-demo/renderer.js
+++ bubble-demo/renderer.js
@@ -1,9 +1,9 @@
-import { v3, m3 } from "./math.js?v=20260807-12";
+import { v3, m3 } from "./math.js?v=20260807-13";
 import {
   createThinFilmLut,
   createFlowNoiseTexture,
   loadHdrTexture
-} from "./optics.js?v=20260807-12";
+} from "./optics.js?v=20260807-13";
 
 const ENVIRONMENTS = [
   "assets/envmap/sunny_vondelpark_4k.hdr"
